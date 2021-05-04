ec5852eebd1c5f66ecab22d282a3aebc1d0041ad
Patrik Votoček <patrik@votocek.cz>
2021-05-04 22:47:20 +0200

    add support for terraform 0.15.x

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
 output "deploy_namespace" {
   value = data.kubernetes_secret.deploy.data.namespace
+  sensitive = true
 }
 
 output "deploy_ca_certifiate" {
   value = data.kubernetes_secret.deploy.data["ca.crt"]
+  sensitive = true
 }
 
 output "deploy_token" {
