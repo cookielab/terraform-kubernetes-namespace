f861d4b78fec8b2df8129c2520fed202c11b67fc
Patrik Votoček <patrik@votocek.cz>
2021-07-21 23:57:16 +0200

    ca and namespace are no-longer sensitive

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,12 @@
 output "deploy_namespace" {
-  value = data.kubernetes_secret.deploy.data.namespace
-  sensitive = true
+  value = nonsensitive(data.kubernetes_secret.deploy.data.namespace)
 }
 
 output "deploy_ca_certifiate" {
-  value = data.kubernetes_secret.deploy.data["ca.crt"]
-  sensitive = true
+  value = nonsensitive(data.kubernetes_secret.deploy.data["ca.crt"])
 }
 
 output "deploy_token" {
-  value = data.kubernetes_secret.deploy.data.token
+  value     = data.kubernetes_secret.deploy.data.token
   sensitive = true
 }
